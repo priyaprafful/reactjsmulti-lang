f64bad1d4b63f3d729d77666da3dfa1beb64a7a6
priyaprafful <priyankamaheshwari.2021@gmail.com>
2020-01-24 16:51:42 +0100

    Error page

---
+++ src/error_page/Error.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+/**
+ *Errror page component
+ */
+const ErrorPage = () => (
+  <div className="error-container">
+    <h4>Something went wrong. </h4>
+    <nav>
+        <ul>
+            <li>
+              Please check your Prismic endpoint configuration in the src/prismic-config.js file. It might be incorrect. 
+            </li>
+            <li>
+              Please check that your Custom Type API ID is correctly defined in the queries. 
+            </li>
+            <li>
+              Check the console for errors.  
+            </li>
+        </ul>
+    </nav>
+  </div>
+);
+
+export default ErrorPage;
