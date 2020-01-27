ae90084549e5e0cb5359b55ca60e2083fe8a55fe
priyaprafful <priyankamaheshwari.2021@gmail.com>
2020-01-27 14:50:14 +0100

    enclosed in one div

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -5,21 +5,14 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-content">
         <p> © 2019 Todoapp-Powered by Prismic</p>
+        <nav className="footer-nav">
+          <ul>
+            <li><img src = "/images/facebook_icon.png" alt = "facebook-icon"/></li>
+            <li><img src = "/images/instagram_icon.png" alt = "instagram-icon"/></li>  
+            <li><img src = "/images/twitter_icon.png" alt = "facebook-icon"/></li>
+          </ul>
+        </nav>
       </div>
-
-      <nav className="footer-nav">
-        <ul>
-          <li>
-            <img src = "/images/facebook_icon.png" alt = "facebook-icon"/>
-          </li>
-          <li>
-            <img src = "/images/instagram_icon.png" alt = "instagram-icon"/>
-          </li>  
-          <li>
-            <img src = "/images/twitter_icon.png" alt = "facebook-icon"/>
-          </li>
-        </ul>
-      </nav>
     </footer>
   )
 } 
