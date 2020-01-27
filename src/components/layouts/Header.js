0a19975362b1000067753f788104746c5dfcbcc2
priyaprafful <priyankamaheshwari.2021@gmail.com>
2020-01-27 14:51:01 +0100

    file path changed and undefined check applied

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import MenuLinks from './MenuLinks';
+import MenuLinks from '../../prismic_pages/MenuLinks';
 
 const Header = ({lang, uid}) => {
+  if(!uid){
+    uid = "";
+  }
+  
   return (
     <header className="header">
       <a href="/" className = "logo-block">
         <img src="/images/check_icon.png"/>
         <h3 className ="logo-text">Todoapp</h3>
       </a>
-       <MenuLinks lang={lang} uid={uid}/>
-   </header>
+      <MenuLinks lang={lang} uid={uid}/>
+    </header>
   );
 }
 
