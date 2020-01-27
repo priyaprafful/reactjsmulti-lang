914cd8350f0bdaab858f7e0aaf9c5f080bdd437f
priyaprafful <priyankamaheshwari.2021@gmail.com>
2020-01-27 16:56:28 +0100

    removed uid prop and add altlanguage

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import MenuLinks from '../../prismic_pages/MenuLinks';
 
-const Header = ({lang, uid}) => {
-  if(!uid){
-    uid = "";
-  }
-  
+const Header = ({lang, altLanguages}) => {
+
   return (
     <header className="header">
       <a href="/" className = "logo-block">
         <img src="/images/check_icon.png"/>
         <h3 className ="logo-text">Todoapp</h3>
       </a>
-      <MenuLinks lang={lang} uid={uid}/>
+      <MenuLinks lang={lang} altLanguages = {altLanguages}/>
     </header>
   );
 }
