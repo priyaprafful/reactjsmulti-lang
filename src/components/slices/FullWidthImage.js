56bfddb99b443c002218f28df010f882694ce26f
priyaprafful <priyankamaheshwari.2021@gmail.com>
2020-01-28 16:49:46 +0100

    no need of redeclare variable name backgroundimage and code identation

--- src/components/slices/FullWidthImage.js
+++ src/components/slices/FullWidthImage.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
 const FullWidthImage = ({ slice }) => {
+  var backgroundImage = "full-width-image-right"
   if (slice.primary.background_image_position) {
-    var backgroundImage = `full-width-image-${slice.primary.background_image_position}`
-  } else {
-    var backgroundImage = "full-width-image-right"
-  }
+    backgroundImage = `full-width-image-${slice.primary.background_image_position}`
+  } 
   return (
-    <section className = "image-container">
-      <div className = {backgroundImage}>
-        <img className = "back-img" src={slice.primary.background_image.url} 
+    <section className="image-container">
+      <div className={backgroundImage}>
+        <img className="back-img" src={slice.primary.background_image.url} 
         alt={slice.primary.background_image.alt} />
       </div>
-      <div className = "main-image">
+      <div className="main-image">
         <img src={slice.primary.layout_image.url} 
-        alt = {slice.primary.layout_image.alt}/>
+        alt = {slice.primary.layout_image.alt} />
       </div>
     </section>
   );
