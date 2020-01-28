71a4e9cbdfce55be94ff3b9b29168b9568f3eaa6
priyaprafful <priyankamaheshwari.2021@gmail.com>
2020-01-28 16:47:22 +0100

    code indentation and combined menulinks page and header page

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
         <p> © 2019 Todoapp-Powered by Prismic</p>
         <nav className="footer-nav">
           <ul>
-            <li><img src = "/images/facebook_icon.png" alt = "facebook-icon"/></li>
-            <li><img src = "/images/instagram_icon.png" alt = "instagram-icon"/></li>  
-            <li><img src = "/images/twitter_icon.png" alt = "facebook-icon"/></li>
+            <li><img src="/images/facebook_icon.png" alt="facebook-icon"/></li>
+            <li><img src="/images/instagram_icon.png" alt="instagram-icon"/></li>  
+            <li><img src="/images/twitter_icon.png" alt="facebook-icon"/></li>
           </ul>
         </nav>
       </div>
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import MenuLinks from '../../prismic_pages/MenuLinks';
+import React, { useEffect, useState } from 'react';
+import { client,linkResolver } from '../../prismic-config';
+import { RichText , Link} from 'prismic-reactjs';
 
-const Header = ({lang, altLanguages}) => {
+const Header = ({ lang, altLanguages }) => {
+  const [menuData, setMenuData] = useState(null);
 
-  return (
-    <header className="header">
-      <a href="/" className = "logo-block">
-        <img src="/images/check_icon.png"/>
-        <h3 className ="logo-text">Todoapp</h3>
-      </a>
-      <MenuLinks lang={lang} altLanguages = {altLanguages}/>
-    </header>
-  );
+  useEffect(() => {
+    const fetchPrismicData = async () => {
+      try {
+        const menuDoc = await client.getSingle('menu', {lang});
+        if (menuDoc) {
+          setMenuData(menuDoc);
+        } else {
+          console.warn('Menu document was not found. Make sure it exists in your Prismic repository');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchPrismicData();
+  }, [lang,altLanguages]);
+
+  if (menuData) {
+    return (
+      <header className="header">
+        <a href="/" className="logo-block">
+          <img src="/images/check_icon.png" alt="website logo" />
+          <h3 className="logo-text">Todoapp</h3>
+        </a>
+        <nav className="main-nav">
+          <ul>
+            {menuData.data.menu_links.map ((item,index) =>{
+              return (
+                <li key={index}>
+                  <a href={Link.url(item.link, linkResolver)}>
+                    {RichText.asText(item.label)}
+                  </a>
+                </li>
+                )
+              })
+            }
+            {altLanguages.map((altLang,index)=>{
+              return (
+                <li key = {index}>
+                  <a href = {linkResolver(altLang)}>
+                    <p className={"flag-icon flag-icon-" + altLang.lang.slice(-2)}></p>
+                  </a> 
+                </li>
+              )
+            })
+            }
+          </ul>
+        </nav>
+      </header>
+    );
+  } 
+  return null 
 }
-
-export default Header;
+export default Header ;
