7fe4ac2fb4ed0787689560d0c58cec29a6807dc8
priyaprafful <priyankamaheshwari.2021@gmail.com>
2020-01-28 16:50:25 +0100

    code identation

--- src/components/slices/HighLightSection.js
+++ src/components/slices/HighLightSection.js
@@ -4,20 +4,20 @@ import { linkResolver } from '../../prismic-config'
 
 const HighLightSection = ({ slice }) => {
   return (
-    <section className = "highlight-section">
-      <div className = "highlight-image">
+    <section className="highlight-section">
+      <div className="highlight-image">
         <picture>
           <source srcSet={slice.primary.highlight_image.Mobileview.url} 
           alt={slice.primary.highlight_image.Mobileview.alt}
-          media='(max-width: 500px)'/>
+          media="(max-width: 500px)" />
           <source srcSet={slice.primary.highlight_image.Tabletview.url} 
           alt={slice.primary.highlight_image.Tabletview.alt}
-          media='(max-width: 1097px)'/>
+          media="(max-width: 1097px)" />
           <img src={slice.primary.highlight_image.url} alt={slice.primary.highlight_image.alt} />
        </picture>
       </div>
 
-      <div className = "highlight-content">
+      <div className="highlight-content">
         <img src={slice.primary.check_icon.url} 
         alt={slice.primary.check_icon.alt} />
         {RichText.render(slice.primary.highlight_title)}
