6dd83c9202e5eca50aa7e736d34fb3e3bb014b9b
priyaprafful <priyankamaheshwari.2021@gmail.com>
2020-01-31 15:55:21 +0100

    api endpoint name changed to 'your-repo-name'

--- src/prismic-config.js
+++ src/prismic-config.js
@@ -2,7 +2,7 @@ import Prismic from 'prismic-javascript'
 // -- Prismic API endpoint
 // Determines which repository to query and fetch data from
 // Configure your site's access point here
-export const apiEndpoint = 'https://reactjsmulti-lang.cdn.prismic.io/api/v2'
+export const apiEndpoint = 'https://your-repo-name.prismic.io/api/v2'
 
 // -- Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
